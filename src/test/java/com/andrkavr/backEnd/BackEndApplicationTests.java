f8db105fbe54d5865e94753b056115803e114fac
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-06-30 12:03:04 +0200

    initial commit

---
+++ src/test/java/com/andrkavr/backEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrkavr.backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
